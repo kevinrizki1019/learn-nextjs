b16ea1fe346302fef5a2377c83ac6d535857b685
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2023-11-28 14:19:13 +0700

    Fix error on table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -87,9 +87,10 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
-                        className="rounded-full"
+												className="rounded-full"
                         width={28}
                         height={28}
+                        alt="Invoice Image"
                       />
                       <p>{invoice.name}</p>
                     </div>
